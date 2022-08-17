bdbd82803c9a6133b8f8ad38644957bcaecbd123
Mohan Raj <Mohan-Raj-Codecs@users.noreply.github.com>
2022-08-17 10:19:03 +0530

    update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+const dates = new Date();
+console.log(dates.getDay());
+console.log(dates.getHours());
+console.log(dates.getMinutes());
+console.log(dates.getDate());
+
 const text = document.querySelector('.text p')
 
 text.innerHTML = text.innerHTML.split("")
@@ -86,97 +92,10 @@ gsap.fromTo('.circle-text' ,{
 })
 
 
-
-
-
-
-
-
 setTimeout(()=>{
-    CounterUpAnimation(num1 , 0 , 100,10)
-    CounterUpAnimation(num2 , 0 , 32 ,10)
-    CounterUpAnimation(num3 , 0 , 50 ,10)
-} ,100)
+    console.log("Changing")
+    CounterUpAnimation(num1 , 0 , 100 , 5000)
+    CounterUpAnimation(num2 , 0 , 32 , 2000)
+    CounterUpAnimation(num3 , 0 , 50 , 3000)
+} , 1000)
 
-window.addEventListener("DOMContentLoaded",() => {
-	const clock = new BouncyBlockClock(".clock");
-});
-
-class BouncyBlockClock {
-	constructor(qs) {
-		this.el = document.querySelector(qs);
-		this.time = { a: [], b: [] };
-		this.rollClass = "clock__block--bounce";
-		this.digitsTimeout = null;
-		this.rollTimeout = null;
-		this.mod = 0 * 60 * 1000;
-
-		this.loop();
-	}
-	animateDigits() {
-		const groups = this.el.querySelectorAll("[data-time-group]");
-
-		Array.from(groups).forEach((group,i) => {
-			const { a, b } = this.time;
-
-			if (a[i] !== b[i]) group.classList.add(this.rollClass);
-		});
-
-		clearTimeout(this.rollTimeout);
-		this.rollTimeout = setTimeout(this.removeAnimations.bind(this),900);
-	}
-	displayTime() {
-		// screen reader time
-		const timeDigits = [...this.time.b];
-		const ap = timeDigits.pop();
-
-		this.el.ariaLabel = `${timeDigits.join(":")} ${ap}`;
-
-		// displayed time
-		Object.keys(this.time).forEach(letter => {
-			const letterEls = this.el.querySelectorAll(`[data-time="${letter}"]`);
-
-			Array.from(letterEls).forEach((el,i) => {
-				el.textContent = this.time[letter][i];
-			});
-		});
-	}
-	loop() {
-		this.updateTime();
-		this.displayTime();
-		this.animateDigits();
-		this.tick();
-	}
-	removeAnimations() {
-		const groups = this.el.querySelectorAll("[data-time-group]");
-	
-		Array.from(groups).forEach(group => {
-			group.classList.remove(this.rollClass);
-		});
-	}
-	tick() {
-		clearTimeout(this.digitsTimeout);
-		this.digitsTimeout = setTimeout(this.loop.bind(this),1e3);	
-	}
-	updateTime() {
-		const rawDate = new Date();
-		const date = new Date(Math.ceil(rawDate.getTime() / 1e3) * 1e3 + this.mod);
-		let h = date.getHours();
-		const m = date.getMinutes();
-		const s = date.getSeconds();
-		const ap = h < 12 ? "AM" : "PM";
-
-		if (h === 0) h = 12;
-		if (h > 12) h -= 12;
-
-		this.time.a = [...this.time.b];
-		this.time.b = [
-			(h < 10 ? `0${h}` : `${h}`),
-			(m < 10 ? `0${m}` : `${m}`),
-			(s < 10 ? `0${s}` : `${s}`),
-			ap
-		];
-
-		if (!this.time.a.length) this.time.a = [...this.time.b];
-	}
-}
